af59aeca78192b82bf62254b43b025fa10b92f4a
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2022-06-01 20:53:59 -0400

    mp/41 ipld dependency (#43)
    * updated lakefile
    
    * fixed `ipldToCid` exception printing

--- lakefile.lean
+++ lakefile.lean
@@ -7,6 +7,6 @@ package Yatima {
   binName := "yatima"
   dependencies := #[{
     name := `Ipld
-    src := Source.git "https://github.com/yatima-inc/Ipld.lean.git" "update-repo"
+    src := Source.git "https://github.com/yatima-inc/Ipld.lean.git" "9d2f32738cffb83e96a07845824cb489a1dcf081"
   }]
 }
