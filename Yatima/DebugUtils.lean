37fe623c82ab1bae08e323724f9b31b0d21a30d0
Hanting Zhang <hantingz@usc.edu>
2022-06-08 22:53:46 -0700

    move debugging to separate file

---
+++ Yatima/DebugUtils.lean
@@ -0,0 +1,56 @@
+import Lean
+
+
+instance : ToString Lean.RecursorRule where
+  toString x := s!"{x.ctor} {x.nfields} {x.rhs}"
+
+def prettyIsUnsafe (x: Bool) : String := if x then "unsafe " else ""
+def prettyDefSafety : Lean.DefinitionSafety -> String
+| .unsafe => "unsafe "
+| .safe => ""
+| .partial => "partial "
+
+instance : ToString Lean.QuotKind where
+  toString x := match x with
+  | .type => "Quot"
+  | .ctor => "Quot.mk"
+  | .lift => "Quot.lift"
+  | .ind => "Quot.ind"
+
+def constantInfoToString : Lean.ConstantInfo -> String
+| .axiomInfo    val =>
+  s!"{prettyIsUnsafe val.isUnsafe}axiom {val.name} {val.levelParams} : {val.type}"
+| .defnInfo     val =>
+  s!"{prettyDefSafety val.safety}def {val.name} {val.levelParams} : {val.type} := 
+  {val.value}"
+| .thmInfo      val =>
+  s!"theorem {val.name} {val.levelParams} : {val.type} :=
+  {val.value}"
+| .opaqueInfo   val =>
+  s!"{prettyIsUnsafe val.isUnsafe}opaque {val.name} {val.levelParams} {val.type} := 
+  {val.value}"
+| .quotInfo     val =>
+  s!"quot {val.name} {val.levelParams} : {val.type} := 
+  {val.kind}"
+| .inductInfo   val =>
+  s!"{prettyIsUnsafe val.isUnsafe}inductive {val.name} {val.levelParams} : {val.type} := 
+  {val.numParams} {val.numIndices} {val.all} {val.ctors} {val.isRec} {val.isUnsafe} {val.isReflexive} {val.isNested}"
+| .ctorInfo     val =>
+  s!"{prettyIsUnsafe val.isUnsafe}constructor {val.name} {val.levelParams} : {val.type} :=
+  {val.induct} {val.cidx} {val.numParams} {val.numFields}"
+| .recInfo      val =>
+  s!"{prettyIsUnsafe val.isUnsafe}recursor {val.name} {val.levelParams} : {val.type} :=
+  {val.all} {val.numParams} {val.numIndices} {val.numMotives} {val.numMinors} {val.rules} {val.k}"
+
+instance bruh : ToString Lean.ConstantInfo where
+  toString := constantInfoToString
+
+#check bruh
+
+instance : ToString Lean.ConstMap where
+  toString cs :=
+    let s := List.map (fun (_, y) => toString y) cs.toList
+    String.join $ List.intersperse "\n" s
+
+def filterUnsafeConstants (cs : Lean.ConstMap) : Lean.ConstMap :=
+  Lean.List.toSMap (List.filter (fun (n, c) => !c.isUnsafe) cs.toList)
