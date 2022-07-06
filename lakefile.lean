e9756c36a874ff1072833b0d65cb97fcb6ee699c
Hanting Zhang <hantingz@usc.edu>
2022-07-05 18:33:30 -0700

    include all transitive dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -8,5 +8,11 @@ package Yatima {
   dependencies := #[{
     name := `Ipld
     src := Source.git "https://github.com/yatima-inc/Ipld.lean.git" "2940e8d8abeda81bd4eda119d12521e477a2490b"
+  }, {
+    name := `LSpec
+    src := Source.git "https://github.com/yatima-inc/LSpec.git" "56da3b774818df05f44d3fc7621a6888b716ee4a"
+  }, {
+    name := "YatimaStdLib",
+    src := Source.git "https://github.com/yatima-inc/YatimaStdLib.lean" "b57d71878e6d9762c75f99b07b4bacdefdadeeaf"
   }]
 }
