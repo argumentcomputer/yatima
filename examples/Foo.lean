79c76703cccdba6f8063ee339f1cab530adb1f58
Hanting Zhang <hantingz@usc.edu>
2022-07-06 15:25:25 -0700

    Problem: Improve the Lean Pretty-Printer (#95)
    * restricting the graph to def constants, only
    
    * draft of mutual inductive block
    
    * rename Yatima.Env to Yatima.Store
    
    * update Inductives with external Recursors for nesting
    
    * refactor and fix errors up to compilation
    
    * killing sorries
    
    * compiling constructors
    
    * compiling recursors
    
    * more progress. pushing because I'm gonna have lunch
    
    * more progress on internal/external recursors
    
    * first pass
    
    * fixing bug from conflicts
    
    * using 'Expr.fix' for constructor types
    
    * solved a bug, but it loops again
    
    * ctorInfo case fix
    
    * fixed loop in 'toYatimaExternalRec' and 'toYatimaInternalRec'
    
    * fix universe issue, but still looping
    
    * refactor to remove `recr` parameter from `toYatimaExpr`
    
    * fix looping
    
    * cleanup
    
    * compiling the entire Foo.lean
    
    * adding more involved inductives to Foo.lean
    
    * renaming 'CompileEnv.order' to 'CompileEnv.recrCtx', also added a nested inductive example to Foo.lean
    
    * a breaking case
    
    * adding more involved inductives to Foo.lean
    
    * improving data model and creating Ipld coe instances for them
    
    * using rust's ipld format for Option
    
    * update frontend
    
    * using 'Sum' notation
    
    * better order and names for 'Const' constructors
    
    * minor constructor order adjustment
    
    * enhancing data model
    
    * some better error messages
    
    * progress on printing
    
    * 2 sorrys left for printing
    
    * minute changes
    
    * recrCtx changes
    
    * add pprint improvements
    
    * more improvements
    
    * env -> store
    
    * save progress but very bad
    
    * it finally works!
    
    * Update Yatima/Compiler/Frontend.lean
    
    Co-authored-by: Arthur Paulino <arthurleonardo.ap@gmail.com>
    Co-authored-by: John C. Burnham <jcb@johnchandlerburnham.com>
    Co-authored-by: Rishikesh Vaishnav <rishhvaishnav@gmail.com>

--- examples/Foo.lean
+++ examples/Foo.lean
@@ -1,6 +1,4 @@
-import Yatima.Name
-
-def foo {α β γ : Type} : α → (β → γ) → α := fun a f => a
+prelude
 
 inductive BLA
   | nil
@@ -8,39 +6,37 @@ inductive BLA
 
 inductive BLAH | blah : BLA → BLAH
 
--- mutual
---   inductive BLE | bli : BLI → BLE
---   inductive BLI | ble : BLE → BLI
---   inductive BLO | blea : BLE → BLA → BLO
--- end
+mutual
+  inductive BLE | bli : BLI → BLE
+  inductive BLI | ble : BLE → BLI
+  inductive BLO | blea : BLE → BLA → BLO
+end
 
--- inductive BLEH
---   | bleh : BLE → BLEH
---   | bloh : BLO → BLEH
+inductive BLEH
+  | bleh : BLE → BLEH
+  | bloh : BLO → BLEH
 
--- mutual
---   inductive Tree (A : Type) where
---     | branch : (a : A) → (trees : TreeList A) → Tree A
+mutual
+  inductive Tree (A : Type) where
+    | branch : (a : A) → (trees : TreeList A) → Tree A
 
---   inductive TreeList (A : Type) where
---     | nil : TreeList A
---     | cons : (t : Tree A) → (ts : TreeList A) → TreeList A
--- end
+  inductive TreeList (A : Type) where
+    | nil : TreeList A
+    | cons : (t : Tree A) → (ts : TreeList A) → TreeList A
+end
 
--- inductive List (α : Type u) where 
---   | nil : List α
---   | cons (a : α) (as : List α) : List α
+inductive List (α : Type u) where 
+  | nil : List α
+  | cons (a : α) (as : List α) : List α
 
--- inductive Treew (A : Type) where
---   | branch : (a : A) → (trees : List (Treew A)) → Treew A
+inductive Treew (A : Type) where
+  | branch : (a : A) → (trees : List (Treew A)) → Treew A
 
--- mutual
---   inductive Treeq (A : Type) where
---     | branch : TreeListq A → (a : A) → (trees : List (Treeq A)) → Treeq A
+mutual
+  inductive Treeq (A : Type) where
+    | branch : TreeListq A → (a : A) → (trees : List (Treeq A)) → Treeq A
 
---   inductive TreeListq (A : Type) where
---     | nil : TreeListq A
---     | cons : (t : Treeq A) → (ts : TreeListq A) → TreeListq A
--- end
-
--- #print TreeList.recOn
+  inductive TreeListq (A : Type) where
+    | nil : TreeListq A
+    | cons : (t : Treeq A) → (ts : TreeListq A) → TreeListq A
+end
