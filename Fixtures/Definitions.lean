b8310751c73619ecb14b7b91765cfa1cae97a73f
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-07-07 20:15:25 +0000

    add mutual inductives test

--- Fixtures/Definitions.lean
+++ Fixtures/Definitions.lean
@@ -1,4 +1,5 @@
 mutual
+  -- ACEF are weakly equal
   unsafe def A : Nat → Nat
   | 0 => 0
   | n + 1 => B n + E n + C n + 1
@@ -20,7 +21,7 @@ mutual
   | n + 1 => B n + C n + E n + 1
 
   -- should be an external mutual block that references into
-  -- the above mutual block
+  -- the above mutual block; GH should be weakly equal
   unsafe def G : Nat → Nat 
   | 0 => 0
   | n + 1 => B n + F n + H n + 2
