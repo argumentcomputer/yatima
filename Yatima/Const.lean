ed8f2a98273a7b4ddba9b59c1002cbe5c6473d8e
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-07-19 22:26:23 +0000

    update `toYatimaInductive`

--- Yatima/Const.lean
+++ Yatima/Const.lean
@@ -268,12 +268,12 @@ match k with
   | .Anon => ⟨(), d.lvls.length, typeCid.anon, valueCid.anon, d.safe⟩
   | .Meta => ⟨d.name, d.lvls, typeCid.meta, valueCid.meta, ()⟩
 
-def Quotient.toIpld {k : Ipld.Kind} (d : Quotient) (typeCid : ExprCid) : Ipld.Quotient k :=
+def Quotient'.toIpld {k : Ipld.Kind} (d : Quotient) (typeCid : ExprCid) : Ipld.Quotient k :=
 match k with
   | .Anon => ⟨(), d.lvls.length, typeCid.anon, d.kind⟩
   | .Meta => ⟨d.name, d.lvls, typeCid.meta, ()⟩
 
-def Axiom.toIpld {k : Ipld.Kind} (d : Axiom) (typeCid : ExprCid) : Ipld.Axiom k :=
+def Axiom'.toIpld {k : Ipld.Kind} (d : Axiom) (typeCid : ExprCid) : Ipld.Axiom k :=
 match k with
   | .Anon => ⟨(), d.lvls.length, typeCid.anon, d.safe⟩
   | .Meta => ⟨d.name, d.lvls, typeCid.meta, ()⟩
