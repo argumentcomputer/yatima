8d121158d011c9ec0ac6d3ece136e5dadb90aafd
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2022-07-20 18:44:49 -0300

    Removed Typechecker conversion from Main.lean
    The conversion is broken as of now, so I will remove it so that we
    don't get "IPLD broken" errors

--- Main.lean
+++ Main.lean
@@ -1,8 +1,5 @@
 import Cli
 import Yatima.Compiler.Frontend
-import Yatima.Typechecker.Infer
-import Yatima.Typechecker.FromIPLD
-import Yatima.Typechecker.Debug
 
 opaque VERSION : String := "0.0.1"
 
@@ -29,7 +26,6 @@ def printCompilationStats (stt : Yatima.Compiler.CompileState) : IO Unit := do
     s!"cache size: {stt.cache.size}\n" ++
     s!"cache: {stt.cache.toList.map fun (n, (_, c)) => (n, c.ctorName)}"
 
-open Yatima.Typechecker in
 open Yatima.Compiler in
 def storeRun (p : Cli.Parsed) : IO UInt32 := do
   let log : Bool := p.hasFlag "log"
@@ -52,8 +48,7 @@ def storeRun (p : Cli.Parsed) : IO UInt32 := do
         return 1
       | none => pure ()
       if log then printCompilationStats stt
-      let exprs ← convertStoreIO stt.store
-      List.forM exprs (fun expr => IO.println s!"{printExpr expr}")
+      -- todo: make use of `stt.store`
       return 0
     else
       IO.eprintln "No store argument was found."
