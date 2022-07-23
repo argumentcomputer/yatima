1d05cfd7cf03028b59c64cf95cbf47774c1250c9
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-23 20:09:31 -0300

    storing timestamps in Cronos using 'Nat' and converting to 'Float' in the summary, only

--- Yatima/Cronos.lean
+++ Yatima/Cronos.lean
@@ -1,8 +1,8 @@
 import YatimaStdLib.RBMap
 
 structure Cronos where
-  refs : Std.RBMap String Float compare
-  data : Std.RBMap String Float compare
+  refs : Std.RBMap String Nat compare
+  data : Std.RBMap String Nat compare
   deriving Inhabited
 
 namespace Cronos
@@ -10,22 +10,19 @@ namespace Cronos
 def new : Cronos :=
   default
 
-def now : IO Float :=
-  return Float.ofNat (← IO.monoMsNow)
-
 variable (c : Cronos)
 
 def clock (tag : String) : IO Cronos := do
-  let now ← now
+  let now ← IO.monoMsNow
   match c.refs.find? tag with
   | none => return { c with refs := c.refs.insert tag now }
   | some ref => return {
     refs := c.refs.insert tag now,
-    data := c.data.insert tag $ (now - ref) / 1000.0 }
+    data := c.data.insert tag (now - ref) }
 
 def summary : String :=
   let timings := c.data.fold (init := "")
-    fun acc tag time => s!"{acc}\n  {tag} | {time}s"
+    fun acc tag time => s!"{acc}\n  {tag} | {(Float.ofNat time) / 1000}s"
   s!"Timings:{timings}"
 
 end Cronos
