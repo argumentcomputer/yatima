31e95123afaa4be67c7dcdc049a0979e74fa190d
mpenciak <matej.penciak@gmail.com>
2022-07-23 00:31:05 -0400

    make runnable test

--- Fixtures/LurkTranslation/SimplePrelude.lean
+++ Fixtures/LurkTranslation/SimplePrelude.lean
@@ -1,3 +1,7 @@
 prelude
 
 def id (a : α) : α := a
+
+inductive test
+  | c1 : test
+  | c2 : test → test
--- Yatima/Transpiler/Test.lean
+++ Yatima/Transpiler/Test.lean
@@ -1,7 +1,17 @@
 import Yatima.Compiler.Frontend
+import Yatima.ForLurkRepo.Printing
+import Yatima.Transpiler.Transpiler
 
-open Yatima.Compiler
+open Yatima.Compiler Lurk
 
 def test : IO Unit := do
-  let _ ← runFrontend "./Fixtures/LurkTranslation/SimplePrelude.lean"
-  IO.println "Print Lurk expr here"
+  match (← runFrontend "./Fixtures/LurkTranslation/SimplePrelude.lean") with 
+    | .ok result => 
+      let compiledStore := result.store
+      match Yatima.Transpiler.transpile compiledStore with
+        | .ok result => IO.println result
+        | .error msg => IO.println msg
+    | .error msg => IO.println msg
+
+-- Last run:
+--(let ((id (lambda (α) (lambda (a) a))) (id:_cstage1 (lambda (α) (lambda (a) a))) (test:recOn (lambda (motive) (lambda (t) (lambda (c1) (lambda (c2) ((((test:rec motive) c1) c2) t))))))) (current-env))
