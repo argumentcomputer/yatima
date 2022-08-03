a4c6e97dab16d65a2decd54955570023b0ca9836
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-08-03 14:04:01 +0000

    MWE

--- Fixtures/AnonCidGroups/ToImport.lean
+++ Fixtures/AnonCidGroups/ToImport.lean
@@ -6,3 +6,11 @@ def Bar := Nat   -- triggering the compilation of `Nat`
 inductive MyOtherNat
   | nada
   | mais : MyOtherNat → MyOtherNat
+
+-- passes
+inductive MyOtherNat' (α : Sort u)
+  | mais : MyOtherNat' α → MyOtherNat' α
+
+-- fails
+inductive MyOtherNat'' (α : Sort u)
+  | mais : α → MyOtherNat'' α
