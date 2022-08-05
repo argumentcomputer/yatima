6d96aa22e6fb0a5a6fcd8efc27e5bc0a9afe8da5
Hanting Zhang <hantingz@usc.edu>
2022-08-05 12:42:43 -0700

    add pip cmd

--- Yatima/Cli/PipeCmd.lean
+++ Yatima/Cli/PipeCmd.lean
@@ -1,7 +1,73 @@
 import Cli
+import Yatima.Cli.CompileCmd 
+import Yatima.Transpiler.Transpiler
+import Lean.Util.Path
 
--- TODO
+open System Yatima.Compiler Yatima.Transpiler in
+def pipeRun (p : Cli.Parsed) : IO UInt32 := do
+  match ← getToolchain with
+  | .error msg => IO.eprintln msg; return 1
+  | .ok toolchain =>
+    if toolchain != Lean.versionString then
+      IO.eprintln
+        s!"Expected toolchain '{Lean.versionString}' but got '{toolchain}'"
+      return 1
+  let log := p.hasFlag "log"
+  let eraseTypes := p.hasFlag "no-erase-types"
+  match p.variableArgsAs? String with
+  | some ⟨args⟩ =>
+    if !args.isEmpty then
+      setLibsPaths
+      let mut stt : CompileState := default
+      let mut errMsg : Option String := none
+      let mut cronos := Cronos.new
+      for arg in args do
+        for filePath in ← getLeanFilePathsList ⟨arg⟩ do
+          let filePathStr := filePath.toString
+          cronos ← cronos.clock filePathStr
+          match ← compile filePath log stt with
+          | .ok stt' => match stt.union stt' with
+            | .ok stt' =>
+              stt := stt'
+              cronos ← cronos.clock filePathStr
+            | .error msg => errMsg := some msg; break
+          | .error msg => errMsg := some (toString msg); break
+        if errMsg.isSome then break
+      match errMsg with
+      | some msg =>
+        IO.eprintln msg
+        return 1
+      | none => pure ()
+      match ← transpile stt with
+      | .error msg => IO.eprintln msg
+      | .ok out => 
+        let path ← IO.currentDir
+        let fname : FilePath := path/ s!"output" |>.withExtension "lurk"
+        IO.FS.writeFile fname s!"{out}" 
+        if p.hasFlag "summary" then
+          IO.println s!"{stt.summary}"
+          IO.println s!"\n{cronos.summary}"
+          IO.println s!"\n{out}"
+      return 0
+    else
+      IO.eprintln "No store argument was found."
+      IO.eprintln "Run `yatima store -h` for further information."
+      return 1
+  | none =>
+    IO.eprintln "Couldn't parse store arguments."
+    IO.eprintln "Run `yatima store -h` for further information."
+    return 1
+
+-- TODO: `no-erase-types` 
 def pipeCmd : Cli.Cmd := `[Cli|
-  pipe NOOP;
-  "Generate Lurk code from Lean code without saving Yatima IR to disk"
+  pipe VIA pipeRun;
+  "Transpile Yatima IR to Lurk code"
+  
+  FLAGS:
+    l, "log";            "Logs transpilation progress"
+    s, "summary";        "Prints a transpilation summary at the end of the process"
+    "no-erase-types";    "Do not erase types from the Yatima source"
+
+  ARGS:
+    ...sources : String; "List of Lean files or directories"
 ]
