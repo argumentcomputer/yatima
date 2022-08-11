1e95822b918a0980b1bc6469e8494d0d2143c0c2
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-08-11 14:59:32 -0300

    move some inductives to the NastyInductives fixture

--- Fixtures/AnonCidGroups/ToImport.lean
+++ Fixtures/AnonCidGroups/ToImport.lean
@@ -6,11 +6,3 @@ def Bar := Nat   -- triggering the compilation of `Nat`
 inductive MyOtherNat
   | nada
   | mais : MyOtherNat → MyOtherNat
-
--- passes
-inductive MyOtherNat' (α : Sort u)
-  | mais : MyOtherNat' α → MyOtherNat' α
-
--- fails
-inductive MyOtherNat'' (α : Sort u)
-  | mais : α → MyOtherNat'' α
