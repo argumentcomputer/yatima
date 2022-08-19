8e51eeee8fa67a1704c946d4bc6accd3b0327458
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-08-19 14:43:10 -0300

    mwe

--- TestsUtils/CompileAndExtractTests.lean
+++ TestsUtils/CompileAndExtractTests.lean
@@ -143,7 +143,7 @@ def typecheckConst (consts : Array Const) (name : Name) : Except String Unit :=
   | .error err => throw $ toString err
 
 def extractPositiveTypecheckTest (stt : CompileState) : TestSeq :=
-  stt.consts.foldl (init := .done) fun tSeq const =>
+  (stt.consts.filter (·.name == `List.recOn)).foldl (init := .done) fun tSeq const =>
     tSeq ++ withExceptOk s!"{const.name} ({const.ctorName}) typechecks"
       (typecheckConst stt.consts const.name) fun _ => .done
 
