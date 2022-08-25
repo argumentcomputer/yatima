94d3b4bdf6a5911c555718cbfde45f64c7c5b60b
Hanting Zhang <hantingz@usc.edu>
2022-08-25 10:14:45 -0700

    fix

--- Yatima/ForLurkRepo/Eval.lean
+++ Yatima/ForLurkRepo/Eval.lean
@@ -2,8 +2,8 @@ import Yatima.ForLurkRepo.DSL
 import Yatima.Compiler.Utils
 
 namespace Lurk
+open Std 
 
--- Maybe something more complex?
 abbrev Frame := Std.RBMap Name Expr compare
 
 abbrev Env := List Frame
@@ -16,6 +16,22 @@ inductive Value where
 
 instance : Inhabited Value where 
   default := .lit .nil
+
+open Std.Format Std.ToFormat in
+partial def Value.pprint (v : Value) (pretty := true) : Std.Format :=
+  match v with 
+    | .lit l => format l 
+    | .lam ns e => 
+      paren <| group ("lambda" ++ line ++ paren (fmtNames ns)) ++ line ++ e.pprint pretty
+    | .raw s => s.pprint pretty
+  where 
+    fmtNames (xs : List Name) := match xs with 
+      | [] => Format.nil
+      | [n]  => format $ fixName n pretty
+      | n::ns => format (fixName n pretty) ++ line ++ fmtNames ns
+
+instance : ToFormat Value where 
+  format := Value.pprint
 
 namespace Interpreter
 
@@ -52,13 +68,16 @@ partial def evalBinOp (op : BinOp) (e₁ e₂ : Expr) (env : Env) : EvalM Value 
     | .lit (.num x), .lit (.num y) => return .lit $ .num (x - y)  
     | _, _ => throw "error: not a number"
   | .prod => match v₁, v₂ with 
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x + y)  
+    | .lit (.num x), .lit (.num y) => return .lit $ .num (x * y)  
     | _, _ => throw "error: not a number"
   | .quot => match v₁, v₂ with 
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x + y)  
+    | .lit (.num x), .lit (.num y) => return .lit $ .num (x * y⁻¹)  
     | _, _ => throw "error: not a number" 
-  | .eq => default
-  | .nEq => default 
+  | .eq => match v₁, v₂ with 
+    | .lit (.num x), .lit (.num y) => 
+      return if x == y then .lit .t else .lit .nil
+    | _, _ => throw "error: not a number"
+  | .nEq => return if v₁ == v₂ then .lit .t else .lit .nil 
 
 partial def evaluate (e : Expr) (env : Env) : EvalM Value := do match e with 
   | .lit lit => return .lit lit 
@@ -83,5 +102,12 @@ def evaluate (e : Expr) (env : Env := default) : Except String Value :=
   match Interpreter.EvalM.run () ⟨env⟩ (Interpreter.evaluate e env) with 
   | .ok res _ => .ok res 
   | .error e _ => .error e
+
+def ppEval (e : Expr) (env : Env := default) : Format := 
+  match evaluate e env with 
+  | .ok res => res.pprint 
+  | .error e => e
+
+#eval ⟦,1⟧
 
 end Lurk 
