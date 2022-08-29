81f53bad401c757f6cf3f302915e495c590449f5
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2022-08-29 20:42:05 +0000

    Update Tests/Termination/NastyInductives.lean
    Co-authored-by: Arthur Paulino <arthurleonardo.ap@gmail.com>

--- Tests/Termination/NastyInductives.lean
+++ Tests/Termination/NastyInductives.lean
@@ -4,7 +4,6 @@ open LSpec in
 def main := do
   let tSeq ← compileAndExtractTests
     "Fixtures/Termination/NastyInductives.lean"
-    -- [extractIpldRoundtripTests/-, extractPositiveTypecheckTests-/]
-    [extractPositiveTypecheckTests]
+    [extractIpldRoundtripTests, extractPositiveTypecheckTests]
     false
   lspecIO tSeq
