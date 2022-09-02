792c771d4ddac355e01b9cac32f1d8e33dfa0a93
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-09-02 12:28:54 -0300

    fix on 'eq' operator; improved error messages

--- Yatima/ForLurkRepo/Eval.lean
+++ Yatima/ForLurkRepo/Eval.lean
@@ -5,14 +5,14 @@ namespace Lurk
 
 open Std
 
-inductive EnvExpr where 
+inductive EnvExpr where
   | mk : (List $ Name × EnvExpr) → Expr → EnvExpr
   deriving Repr, BEq
 
-def EnvExpr.env (lb : EnvExpr) : (List $ Name × EnvExpr) := match lb with
+def EnvExpr.env : EnvExpr → (List $ Name × EnvExpr)
   | .mk env _ => env
 
-def EnvExpr.expr (lb : EnvExpr) : Expr := match lb with
+def EnvExpr.expr : EnvExpr → Expr
   | .mk _ expr => expr
 
 inductive Value where
@@ -22,70 +22,74 @@ inductive Value where
   | env   : List (Name × Value) → Value
   deriving Repr, BEq, Inhabited
 
+notation "TRUE"  => Value.lit Literal.t
+notation "FALSE" => Value.lit Literal.nil
+
 open Format ToFormat in
 partial def Value.pprint (v : Value) (pretty := true) : Format :=
   match v with
     | .lit l => format l
     | .lam ns _ lb => paren $
       group ("lambda" ++ line ++ paren (fmtNames ns)) ++ line ++ lb.expr.pprint pretty
-    | e@(.cons ..) => 
+    | e@(.cons ..) =>
       let (es, tail) := telescopeCons [] e
-      let tail := if tail == .lit .nil then 
-        Format.nil 
-      else 
+      let tail := if tail == FALSE then
+        Format.nil
+      else
         line ++ "." ++ line ++ pprint tail pretty
       paren <| fmtList es ++ tail
     | .env e => paren <| fmtEnv e
-  where 
-    telescopeCons (acc : List Value) (e : Value) : List Value × Value := match e with 
+  where
+    telescopeCons (acc : List Value) (e : Value) : List Value × Value := match e with
       | .cons e₁ e₂ => telescopeCons (e₁ :: acc) e₂
       | _ => (acc.reverse, e)
-    fmtNames (xs : List Name) := match xs with 
+    fmtNames (xs : List Name) := match xs with
       | [ ]   => Format.nil
       | [n]   => format (fixName n pretty)
       | n::ns => format (fixName n pretty) ++ line ++ fmtNames ns
-    fmtList (xs : List Value) := match xs with 
+    fmtList (xs : List Value) := match xs with
       | [ ]   => Format.nil
       | [n]   => format (pprint n pretty)
       | n::ns => format (pprint n pretty) ++ line ++ fmtList ns
-    fmtEnv (xs : List (Name × Value)) := match xs with 
+    fmtEnv (xs : List (Name × Value)) := match xs with
       | []         => Format.nil
-      | [(n, v)]   => 
+      | [(n, v)]   =>
         format (fixName n pretty) ++ line ++ "." ++ line ++ format (pprint v pretty)
-      | (n, v)::ns => 
+      | (n, v)::ns =>
         format (fixName n pretty) ++ line ++ "." ++ line ++ format (pprint v pretty) ++ line ++ fmtEnv ns
 
-instance : ToFormat Value where 
-  format := Value.pprint
+instance : ToFormat Value := ⟨Value.pprint⟩
+
+instance : ToString Value where
+  toString x := toString x.pprint
 
 abbrev EvalM := ExceptT String IO
 
 abbrev Env := RBMap Name (EnvExpr × EvalM Value) compare
 
-def Env.getEnvExpr (env : Env) (e : Expr) : EnvExpr := ⟨env.toList.map fun (n, (lb, _)) => (n, lb), e⟩
+def Env.getEnvExpr (env : Env) (e : Expr) : EnvExpr :=
+  ⟨env.toList.map fun (n, (lb, _)) => (n, lb), e⟩
 
-def evalBinaryOp (op : BinaryOp) (v₁ v₂ : Value) : EvalM Value :=
-  match op with
-  | .sum => match v₁, v₂ with
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x + y)
-    | _, _ => throw "error: not a number"
-  | .diff => match v₁, v₂ with
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x - y)
-    | _, _ => throw "error: not a number"
-  | .prod => match v₁, v₂ with
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x * y)
-    | _, _ => throw "error: not a number"
-  | .quot => match v₁, v₂ with
-    | .lit (.num x), .lit (.num y) => return .lit $ .num (x * y⁻¹)
-    | _, _ => throw "error: not a number"
+def num! : Value → EvalM (Fin N)
+  | .lit (.num x) => pure x
+  | v => throw s!"expected numerical value, got\n {v}"
+
+instance : Coe Bool Value where coe
+  | true  => TRUE
+  | false => FALSE
+
+def evalBinaryOp (v₁ v₂ : Value) : BinaryOp → EvalM Value
+  | .sum  => return .lit $ .num $ (← num! v₁) + (← num! v₂)
+  | .diff => return .lit $ .num $ (← num! v₁) - (← num! v₂)
+  | .prod => return .lit $ .num $ (← num! v₁) * (← num! v₂)
+  | .quot => return .lit $ .num $ (← num! v₁) * (← num! v₂)⁻¹
   | .eq => match v₁, v₂ with
-    | .lit (.num x), .lit (.num y) =>
-      return if x == y then .lit .t else .lit .nil
-    | _, _ => throw "error: not a number"
-  | .nEq => return if v₁ == v₂ then .lit .t else .lit .nil
+    | .lit l₁, .lit l₂ => return l₁ == l₂
+    | _, _ => return FALSE
+  | .nEq => return v₁ == v₂
 
 def bind (ns : List Name) (as : List Expr) :
-    EvalM ((List (Name × Expr)) × List Name) := do
+    EvalM ((List (Name × Expr)) × List Name) :=
   let rec aux (acc : List (Name × Expr)) :
       List Name → List Expr → EvalM ((List (Name × Expr)) × List Name)
     | n::ns, a::as => aux ((n, a) :: acc) ns as
@@ -94,9 +98,11 @@ def bind (ns : List Name) (as : List Expr) :
   aux [] ns as
 
 mutual
+
 -- Reproduce the environment needed to evaluate an `EnvExpr`.
 partial def envExprToEnv (envExpr : EnvExpr) : Env :=
-  envExpr.env.foldl (init := default) fun acc (n, envExpr') => acc.insert n $ (envExpr', eval (envExprToEnv envExpr') envExpr'.expr)
+  envExpr.env.foldl (init := default) fun acc (n, envExpr') =>
+    acc.insert n $ (envExpr', eval (envExprToEnv envExpr') envExpr'.expr)
 
 partial def eval (env : Env) : Expr → EvalM Value
   | .lit lit => return .lit lit
@@ -105,9 +111,9 @@ partial def eval (env : Env) : Expr → EvalM Value
     | none => throw s!"{n} not found"
   | .ifE tst con alt => do
     match ← eval env tst with
-    | .lit .t => eval env con
-    | .lit .nil => eval env alt
-    | _ => throw "not a boolean"
+    | TRUE  => eval env con
+    | FALSE => eval env alt
+    | v => throw s!"expected boolean value, got\n {v}"
   | .lam formals body => return .lam formals [] $ env.getEnvExpr body
   | .letE bindings body => do
     let env' ← bindings.foldlM (init := env)
@@ -133,7 +139,7 @@ partial def eval (env : Env) : Expr → EvalM Value
         -- FIXME "some ee"
         let (env, _) ← (patch.map (·, none) ++ (lb.env.map (fun (n, ee) => ((n, ee.expr), some ee))) : List ((Name × Expr) × Option EnvExpr)).reverse.foldlM (init := (default, env))
           fun (acc, acc') ((n, e), env?) => do
-            let env := 
+            let env :=
               match env? with
               -- arguments are free to use the current context
               | none => acc'
@@ -146,46 +152,46 @@ partial def eval (env : Env) : Expr → EvalM Value
         -- a lambda body should be evaluated in the context of *its arguments alone* (plus whatever context it originally had)
         eval env lb.expr
       else return .lam ns' patch lb
-    | .env env => 
+    | .env env =>
       if args.isEmpty then return .env env else throw "too many arguments"
-    | _ => throw "app function is not a lambda"
+    | v => throw s!"expected lambda value, got\n {v}"
   | .quote _ => throw "`quote` is currently not supported"
-  | .binaryOp op e₁ e₂ => do evalBinaryOp op (← eval env e₁) (← eval env e₂)
+  | .binaryOp op e₁ e₂ => do evalBinaryOp (← eval env e₁) (← eval env e₂) op
   | .atom e => return match ← eval env e with
-    | .cons .. => .lit .t
-    | _ => .lit .nil
+    | .cons .. => TRUE
+    | _ => FALSE
   | .cons e₁ e₂ => return .cons (← eval env e₁) (← eval env e₂)
   | .strcons e₁ e₂ => do match (← eval env e₁), (← eval env e₂) with
     | .lit (.char c), .lit (.str s) => return .lit (.str ⟨c :: s.data⟩)
-    | .lit (.char _), x => throw s!"expected string value, got\n {x.pprint}"
-    | x, _ => throw s!"expected char value, got\n {x.pprint}"
+    | .lit (.char _), v => throw s!"expected string value, got\n {v}"
+    | v, _ => throw s!"expected char value, got\n {v}"
   | .car e => do match ← eval env e with
     | .cons v _ => return v
     | .lit (.str s) => match s.data with
       | c::_ => return .lit $ .char c
-      | [] => return .lit .nil
-    | _ => throw "not a cons"
+      | [] => return FALSE
+    | v => throw s!"expected cons value, got\n {v}"
   | .cdr e => do match ← eval env e with
     | .cons _ v => return v
     | .lit (.str s) => match s.data with
       | _::cs => return .lit $ .str ⟨cs⟩
       | [] => return .lit $ .str ""
-    | _ => throw "not a cons"
+    | v => throw s!"expected cons value, got\n {v}"
   | .emit e => do
     let v ← eval env e
-    IO.println v.pprint
+    IO.println v
     pure v
   | .begin es => eval env $ es.reverse.headD $ .lit .nil
   | .currEnv => do
     let mut ret := default
     for (n, (_, e)) in env do
-      ret := (n, ←e) :: ret
+      ret := (n, ← e) :: ret
     return .env ret
 end
 
 def ppEval (e : Expr) (env : Env := default) : IO Format :=
   return match ← eval env e with
-  | .ok res => res.pprint 
+  | .ok res => res.pprint
   | .error e => e
 
 #eval ppEval ⟦(letrec ((exp (lambda (base exponent)
