6d85bc8a49b30c9883d470764223eddddd0aeeeb
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-09-27 16:02:26 -0300

    plug deserialization call into CLI

--- Yatima/Cli/GetCmd.lean
+++ Yatima/Cli/GetCmd.lean
@@ -1,6 +1,11 @@
 import Cli
+import Yatima.Ipld.FromIpld
 
 def dagGet (_p : Cli.Parsed) : IO UInt32 := do
+  let ipld : Ipld := sorry
+  match Yatima.Ipld.storeFromIpld ipld with
+  | some _ => IO.println "deserialization succeeded"
+  | none => IO.println "deserialization failed"
   return 0
 
 def getCmd : Cli.Cmd := `[Cli|
