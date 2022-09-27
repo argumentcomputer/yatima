4e3a636e2bf1ab18c51dabaca68b68a522b9c249
Hanting Zhang <hantingz@usc.edu>
2022-09-27 16:20:46 -0700

    initial commit

--- Yatima/Cli/VerifyCmd.lean
+++ Yatima/Cli/VerifyCmd.lean
@@ -1,7 +1,24 @@
-import Cli
+import Yatima.Cli.PipeCmd
+
+open System Yatima.Compiler Yatima.Typechecker Yatima.Transpiler Cli.Parsed in
+def verifyRun (p : Cli.Parsed) : IO UInt32 := do _
 
 -- TODO
 def verifyCmd : Cli.Cmd := `[Cli|
-  verify NOOP;
-  "Verify correctness of a Lurk proof"
+  pipe VIA verifyRun;
+  "Transpile Lean 4 code to Lurk code"
+  
+  FLAGS:
+    p, "prelude"; "Optimizes the compilation of prelude files without imports." ++
+      " All files to be compiled must follow this rule"
+    l, "log";             "Logs compilation progress"
+    s, "summary";         "Prints a compilation summary at the end of the process"
+    ty, "typecheck";      "Typechecks the Yatima IR code"
+    rt, "root" : String;  "Sets the root call for the Lurk evaluation (defaults to `root`)"
+    o, "output" : String; "Specifies the target file name for the Lurk code"
+    r, "run";             "Runs the evaluation of the resulting Lurk expression"
+    "no-erase-types";     "Do not erase types from the Yatima source"
+
+  ARGS:
+    ...sources : String; "List of Lean files or directories"
 ]
