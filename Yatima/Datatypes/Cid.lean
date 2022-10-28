4164c85cdf027c4778802581174c233fd397438c
mpenciak <matej.penciak@gmail.com>
2022-10-28 15:07:10 -0400

    small Yatima.Datatypes fixes

--- Yatima/Datatypes/Cid.lean
+++ Yatima/Datatypes/Cid.lean
@@ -4,21 +4,21 @@ import Yatima.Datatypes.Kind
 namespace Yatima.IR
 
 /-- Constants to encode universe levels in IPLD -/
-@[matchPattern] def UNIV : Kind → UInt64
+@[match_pattern] def UNIV : Kind → UInt64
   | .anon => 0xC0DE0001
   | .meta => 0xC0DE0002
 
 /-- Constants to encode expressions in IPLD -/
-@[matchPattern] def EXPR : Kind → UInt64
+@[match_pattern] def EXPR : Kind → UInt64
   | .anon => 0xC0DE0003
   | .meta => 0xC0DE0004
 
 /-- Constants to encode constants in IPLD -/
-@[matchPattern] def CONST : Kind → UInt64
+@[match_pattern] def CONST : Kind → UInt64
   | .anon => 0xC0DE0005
   | .meta => 0xC0DE0006
 
-@[matchPattern] def STORE : UInt64 := 0xC0DE0007
+@[match_pattern] def STORE : UInt64 := 0xC0DE0007
 
 structure UnivCid  (k : Kind) where data : Cid deriving BEq, Ord, Inhabited, Repr
 structure ExprCid  (k : Kind) where data : Cid deriving BEq, Ord, Inhabited, Repr
