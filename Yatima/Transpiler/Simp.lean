490416cfc1dad2c437c96f92c30594e56605cbc5
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-17 18:49:53 -0300

    fix the Nat lit simplifier

--- Yatima/Transpiler/Simp.lean
+++ Yatima/Transpiler/Simp.lean
@@ -4,7 +4,7 @@ import Lurk.Syntax.AST
 # Simp
 A rudimentary simplifier for lurk expressions. 
 * Current we only plan to support things like 
-  `(((OfNat.ofNat Nat) n) (instOfNatNat n)) => n`
+  `(OfNat.ofNat Nat n (instOfNatNat n)) => n`
 * There is a more principled way to do this, 
   so this file should be taken as a hack and not 
   built upon heavily.
@@ -13,10 +13,8 @@ namespace Yatima.Transpiler.Simp
 
 open Lurk.Syntax AST in
 def getOfNatLit : AST → AST
-  | x@~[
-      ~[~[sym "OfNat.ofNat", sym "Nat"], num n], -- ((OfNat.ofNat Nat) n)
-      ~[sym "instOfNatNat", num m]               -- (instOfNatNat n)
-    ] => if n == m then num n else x
+  | x@~[sym "OfNat.ofNat", sym "Nat", num n, ~[sym "instOfNatNat", num m]] =>
+    if n == m then num n else x
   | x => x
 
 end Yatima.Transpiler.Simp
