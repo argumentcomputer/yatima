b918eb53866c29d7889b4700f423e92fe30c6f2b
Hanting Zhang <hantingz@usc.edu>
2022-12-16 11:59:43 -0800

    remove another test that needs `commit`

--- Tests/Transpilation/TrickyTypes.lean
+++ Tests/Transpilation/TrickyTypes.lean
@@ -7,8 +7,8 @@ def main := do
   let tSeq ← extractTranspilationTests
     "Fixtures/Transpilation/TrickyTypes.lean"
     [ ("exprCtor", "lam"),
-      ("univCtor", "zero"),
+      -- ("univCtor", "zero"), -- TODO: needs `commit` on `Lurk.lean`
       ("treeSize", 2)
-      -- ("nameStr",  "this.is.a.name") -- needs `commit` on `Lurk.lean`
+      -- ("nameStr",  "this.is.a.name") -- TODO: needs `commit` on `Lurk.lean`
     ]
   lspecIO tSeq
