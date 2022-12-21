5a659a028afd473111a1526850cff8b32aa79af9
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-12-21 20:21:20 -0300

    fix args order for proj

--- lakefile.lean
+++ lakefile.lean
@@ -25,7 +25,7 @@ require Cli from git
   "https://github.com/yatima-inc/Cli.lean" @ "cd523a1951a8ec1ffb276446280ac60a7c5ad333"
 
 require Lurk from git
-  "https://github.com/yatima-inc/Lurk.lean" @ "ed8cd1a37c23d1de5f9901ebc6bbb68fad82f56d"
+  "https://github.com/yatima-inc/Lurk.lean" @ "75aa04edfab54ed0e9211ae6558999f4462e9ce7"
 
 require std from git
   "https://github.com/leanprover/std4/" @ "d83e97c7843deb1cf4a6b2a2c72aaf2ece0b4ce8"
