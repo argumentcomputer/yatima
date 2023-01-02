34fdb14f137678fb19cdabe8dae1431d940a3a18
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2023-01-02 10:17:40 -0500

    remove rust code and move to radiya.rs (#234)
    This, paired with the https://github.com/yatima-inc/radiya.rs/pull/7
    pull request moves all the Rust code from this repo to the `radiya.rs`
    repo.

--- yatima-rs/src/parse.rs
+++
@@ -1,9 +0,0 @@
-pub mod base;
-pub mod constant;
-pub mod error;
-pub mod expr;
-pub mod span;
-pub mod string;
-pub mod univ;
-pub mod utils;
-pub mod env;
--- yatima-rs/src/typechecker.rs
+++
@@ -1,7 +0,0 @@
-pub mod check;
-pub mod from_anon;
-pub mod equality;
-pub mod evaluation;
-pub mod expression;
-pub mod universe;
-pub mod value;
