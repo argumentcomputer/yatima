0ca73cccf572cae41a9922e971d33799d9e2665a
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2023-01-23 20:34:22 -0300

    Finished Grin AST

--- Yatima/Compiler/Grin.lean
+++ Yatima/Compiler/Grin.lean
@@ -1,16 +1,61 @@
+import YatimaStdLib.NonEmpty
+
+-- Based on https://nbviewer.org/github/grin-compiler/grin/blob/master/papers/boquist.pdf
 namespace Grin
+
+abbrev Var := String
+abbrev Id := String
 
 inductive Tag where
 -- Suspended full applications
-| F : Tag
+| F : Id → Tag
 -- Partial applications
-| P : Tag
+| P : Id → Tag
 -- Saturated constructors
-| C : Tag
+| C : Id → Tag
 
-inductive Binding where
+inductive SVal where
+  | str : String → SVal
+  | num : Nat → SVal
+  | var : Var → SVal
 
-inductive Program where
-| bindings : Binding → List Binding → Program
+inductive Val where
+  | ctag : Tag → List SVal → Val
+  | cvar : Var → List SVal → Val
+  | stag : Tag → Val
+  | empty : Val
+  | sval : SVal → Val
+
+inductive CPat where
+  | ctag : Tag → List Var → CPat
+  | stag : Tag → CPat
+  | str : String → CPat
+  | num : Nat → CPat
+
+abbrev LPat := Val
+
+mutual
+inductive Expr where
+  | seq : SExpr → LPat → Expr → Expr
+  | case : Val → NEList (CPat × Expr) → Expr
+  | «if» : Val → Expr → Expr → Expr
+  | op : SExpr → Expr
+
+inductive SExpr where
+  | app : Var → NEList SVal → SExpr
+  | unit : Val → SExpr
+  | store : Val → SExpr
+  | fetch : Var → Option Nat → SExpr
+  | update : Var → Val → SExpr
+  | expr : Expr → SExpr
+end
+
+structure Binding where
+  defn : Var
+  args : NEList Var
+  body : Expr
+  
+
+abbrev Prog := NEList Binding 
 
 end Grin
