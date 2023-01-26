368f69ca1c2f3e9f8b9500cdb7113ac014f609d3
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2023-01-26 12:17:17 -0300

    Redefinition of Grin AST

--- Yatima/Compiler/Grin.lean
+++ Yatima/Compiler/Grin.lean
@@ -3,58 +3,66 @@ import YatimaStdLib.NonEmpty
 -- Based on https://nbviewer.org/github/grin-compiler/grin/blob/master/papers/boquist.pdf
 namespace Grin
 
-abbrev Var := String
-abbrev Id := String
+/-- Var represents a variable binding, such as in a function definition in a pattern -/
+structure Var where
+  data : Lean.FVarId
+/-- Id represents a known definition, such as a function or constructor -/
+structure Id where
+  data : Lean.FVarId
 
 inductive Tag where
 -- Suspended full applications
 | F : Id → Tag
 -- Partial applications
-| P : Id → Tag
+| P : Id → Nat → Tag
 -- Saturated constructors
 | C : Id → Tag
 
+inductive Literal
+  | str : String → Literal
+  | num : Nat → Literal
+
 inductive SVal where
-  | str : String → SVal
-  | num : Nat → SVal
+  | lit : Literal → SVal
   | var : Var → SVal
 
 inductive Val where
-  | ctag : Tag → List SVal → Val
-  | cvar : Var → List SVal → Val
-  | stag : Tag → Val
+  | ctag  : Tag → List SVal → Val
+  | cvar  : Var → List SVal → Val
+  | stag  : Tag → Val
   | empty : Val
-  | sval : SVal → Val
+  | sval  : SVal → Val
 
 inductive CPat where
+  -- Complete tag pattern
   | ctag : Tag → List Var → CPat
+  -- Single tag pattern
   | stag : Tag → CPat
-  | str : String → CPat
-  | num : Nat → CPat
+  | lit  : Literal → CPat
 
 abbrev LPat := Val
 
-mutual
+inductive SExpr where
+  | store  : Val → SExpr
+  | fetch  : Var → Option Nat → SExpr
+  | update : Var → Val → SExpr
+  -- Known function call
+  | call   : Id → NEList SVal → SExpr
+  -- Evaluation of unknown expression
+  | eval   : Var → SExpr
+  -- Application of unknown function to a list of arguments
+  | apply  : Var → NEList SVal → SExpr
+
 inductive Expr where
-  | seq : SExpr → LPat → Expr → Expr
+  | unit : Val → Expr
+  | seq  : SExpr → LPat → Expr → Expr
   | case : Val → NEList (CPat × Expr) → Expr
   | «if» : Val → Expr → Expr → Expr
-  | op : SExpr → Expr
-
-inductive SExpr where
-  | app : Var → NEList SVal → SExpr
-  | unit : Val → SExpr
-  | store : Val → SExpr
-  | fetch : Var → Option Nat → SExpr
-  | update : Var → Val → SExpr
-  | expr : Expr → SExpr
-end
 
 structure Binding where
   defn : Var
   args : NEList Var
   body : Expr
-  
 
 abbrev Prog := NEList Binding 
 
