2954c5e8dc96c4a4cd84fb18bdaa60025ad5d115
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2023-01-28 11:16:25 -0300

    Conversion WIP

--- Yatima/Compiler/Grin.lean
+++ Yatima/Compiler/Grin.lean
@@ -1,4 +1,6 @@
 import YatimaStdLib.NonEmpty
+import Lean.Expr
+import Lean.Compiler.LCNF
 
 -- Based on https://nbviewer.org/github/grin-compiler/grin/blob/master/papers/boquist.pdf
 namespace Grin
@@ -8,7 +10,7 @@ structure Var where
   data : Lean.FVarId
 /-- Id represents a known definition, such as a function or constructor -/
 structure Id where
-  data : Lean.FVarId
+  data : Lean.Name
 
 inductive Tag where
 -- Suspended full applications
@@ -28,7 +30,6 @@ inductive SVal where
 
 inductive Val where
   | ctag  : Tag → List SVal → Val
-  | cvar  : Var → List SVal → Val
   | stag  : Tag → Val
   | empty : Val
   | sval  : SVal → Val
@@ -40,7 +41,11 @@ inductive CPat where
   | stag : Tag → CPat
   | lit  : Literal → CPat
 
-abbrev LPat := Val
+inductive LPat where
+  | ctag  : Tag → List SVal → LPat
+  | cvar  : Var → List SVal → LPat
+  | stag  : Tag → LPat
+  | svar  : Var → LPat
 
 inductive SExpr where
   | store  : Val → SExpr
@@ -55,15 +60,36 @@ inductive SExpr where
 
 inductive Expr where
   | unit : Val → Expr
-  | seq  : SExpr → LPat → Expr → Expr
+  | seq  : Expr → LPat → Expr → Expr
+  | op   : SExpr → LPat → Expr → Expr
   | case : Val → NEList (CPat × Expr) → Expr
   | «if» : Val → Expr → Expr → Expr
 
 structure Binding where
-  defn : Var
+  defn : Id
   args : NEList Var
   body : Expr
 
 abbrev Prog := NEList Binding 
+
+open Lean.Compiler
+
+def Expr.fromLCNF : LCNF.Code → Expr
+| .let decl k => sorry
+| .fun decl k => sorry
+| .jp decl k => sorry
+| .jmp fvarId args => sorry
+| .cases cases => sorry
+| .return fvarId => sorry
+| .unreach type => sorry
+
+def Binding.fromLCNF (decl : LCNF.Decl) : Binding :=
+  let defn := ⟨decl.name⟩
+  match decl.params.toList.map (fun param => Var.mk param.fvarId) with
+  | [] => sorry
+  | arg :: args =>
+    let args := arg :| args
+    let body := sorry
+    { defn, args, body }
 
 end Grin
