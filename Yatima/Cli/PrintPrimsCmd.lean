6b28a20d4a0536ef3bb57fc6dad94904f38580aa
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-02-05 11:49:13 -0300

    pp command can write Lean code!

--- Yatima/Cli/PrintPrimsCmd.lean
+++ Yatima/Cli/PrintPrimsCmd.lean
@@ -16,9 +16,27 @@ def primsInput : String :=
     (s!"def x{i} := {name}" :: l, i + 1)
   "\n".intercalate defs
 
-def formatMatchesP2F (pairs : List (Lean.Name × Lurk.F)) : List String := sorry
+def nameToPrimRepr : Lean.Name → String
+  | ``Nat        => ".nat"
+  | ``Nat.zero   => ".natZero"
+  | ``Bool       => ".bool"
+  | ``Bool.true  => ".boolTrue"
+  | ``Bool.false => ".boolFalse"
+  | ``String     => ".string"
+  | ``Nat.add    => ".op .natAdd"
+  | ``Nat.mul    => ".op .natMul"
+  | ``Nat.pow    => ".op .natPow"
+  | ``Nat.beq    => ".op .natBeq"
+  | ``Nat.blt    => ".op .natBlt"
+  | ``Nat.ble    => ".op .natBle"
+  | ``Nat.succ   => ".op .natSucc"
+  | x => panic! s!"Invalid name: {x}"
 
-def formatMatchesF2P (pairs : List (Lean.Name × Lurk.F)) : List String := sorry
+def formatMatchesP2F (pairs : List (Lean.Name × Lurk.F)) : List String :=
+  pairs.map fun (n, f) => s!"  | {nameToPrimRepr n} => return .ofNat {f.val}"
+
+def formatMatchesF2P (pairs : List (Lean.Name × Lurk.F)) : List String :=
+  pairs.map fun (n, f) => s!"  | .ofNat {f.val} => return {nameToPrimRepr n}"
 
 def targetFile : FilePath :=
   "Yatima" / "Typechecker" / "TypecheckM.lean"
@@ -27,13 +45,12 @@ def printPrimsRun (_p : Cli.Parsed) : IO UInt32 := do
   Lean.setLibsPaths
   let leanEnv ← Lean.runFrontend primsInput default
   let (constMap, delta) := leanEnv.getConstsAndDelta
-  let delta := delta.filter (primConstNames.contains ·.name)
   let caStt ← match contAddr constMap delta default with
     | .error err => IO.eprintln err; return 1
     | .ok stt => pure stt
   let anonHashes := caStt.env.consts.foldl (init := #[]) fun acc name (h, _) =>
     if primConstNames.contains name then acc.push h else acc
-  
+
   let store := caStt.storeAnon
 
   let commits ← match ← commit anonHashes store false false with
@@ -48,19 +65,19 @@ def printPrimsRun (_p : Cli.Parsed) : IO UInt32 := do
     "\n".intercalate (formatMatchesP2F $ decls.zip commits) ++ "\n"
 
   let fToPrim := "def fToPrim : F → Option PrimConst\n" ++
-    "\n".intercalate (formatMatchesF2P $ decls.zip commits) ++ "\n"
+    "\n".intercalate (formatMatchesF2P $ decls.zip commits) ++ "\n  | _ => none\n"
 
   let primToFQuick := "def primToFQuick : PrimConst → Option F\n" ++
     "\n".intercalate (formatMatchesP2F $ decls.zip commitsQuick) ++ "\n"
 
   let fToPrimQuick := "def fToPrimQuick : F → Option PrimConst\n" ++
-    "\n".intercalate (formatMatchesF2P $ decls.zip commitsQuick) ++ "\n"
+    "\n".intercalate (formatMatchesF2P $ decls.zip commitsQuick) ++ "\n  | _ => none\n"
 
   match (← IO.FS.readFile targetFile).splitOn "--PRIMBEGIN" with
   | [beg, en] => match en.splitOn "--PRIMEND" with
     | [_, en] =>
       let content :=
-        beg ++ "--PRIMBEGIN" ++
+        beg ++ "--PRIMBEGIN\n" ++
         primFoF ++ fToPrim ++ primToFQuick ++ fToPrimQuick ++
         "--PRIMEND" ++ en
       IO.FS.writeFile targetFile content
