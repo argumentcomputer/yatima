f96f219c7ae50c91deedd4511f0af1e82d46bc45
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-27 16:53:47 -0300

    fix dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -21,7 +21,7 @@ require Cli from git
   "https://github.com/lurk-lab/Cli.lean" @ "ef6f9bcd1738638fca8d319dbee653540d56614e"
 
 require Lurk from git
-  "https://github.com/yatima-inc/Lurk.lean" @ "29a622e4cf93017acc215d5e1f154b1c5f53e4b4"
+  "https://github.com/lurk-lab/Lurk.lean" @ "29a622e4cf93017acc215d5e1f154b1c5f53e4b4"
 
 require LightData from git
   "https://github.com/lurk-lab/LightData" @ "6dfd01c9e056deaf5b76e20f995c39e840bbde86"
